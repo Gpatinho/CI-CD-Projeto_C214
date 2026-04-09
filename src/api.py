7072e28256ada163cf176ee6bc75ae29e9b4882b
Gpatinho <guilhermefribeiro41@gmail.com>
2026-04-09 16:44:26 -0300

    fix: add host 0.0.0.0 no api.py para funcionar no container

--- src/api.py
+++ src/api.py
@@ -67,4 +67,4 @@ def deletar(matricula):
 if __name__ == '__main__':
     init_db()
     sincronizar_contadores()
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000, host='0.0.0.0')
